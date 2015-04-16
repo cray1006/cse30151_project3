1ca19d1ea6735bd77d593ca0ab845674870666a6
Christopher William Ray <cray@student00.cse.nd.edu>
2015-04-16 00:33:49 -0400

    second commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python -tt
+
+#Christopher Ray and Dinh Do
+#Professor Blanton 
+#CSE 30151
+#13 April 2015
+
+#main.py
+#main driver program for Turing machine simulator
+
+def main():
+	print "Hello World"
+
+
+if __name__ == '__main__':
+	main()
